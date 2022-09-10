b3d312ad11ebaf6ad28af416a3e297145d8e00ec
Tisotti <yana.albertivna@gmail.com>
2022-09-11 00:25:11 +0200

    Update repetaM3Z6.js

---
+++ js/repetaM3Z6.js
@@ -0,0 +1,24 @@
+//                Objects 
+//      spred 
+// ... это распыление или распаковка
+// примитивы распыляются  в копии
+// а на сложные элементы (объекты ставится ссылка)
+// можно распылить итеррируемые объекты в том числе строки 
+
+// сшиваем три массива spred
+
+// const lastWeekTemp = [1, 2, 3];
+// const currentWeekTemp = [4, 5, 6];
+// const nextWeekTemp = [7, 8, 9];
+
+// const totalTemp = [...lastWeekTemp, ...currentWeekTemp, ...nextWeekTemp];
+
+// console.log(totalTemp);
+
+
+// const totalTempRevers = [...currentWeekTemp, ...lastWeekTemp,  ...nextWeekTemp];
+
+// console.log(totalTempRevers);
+
+// -----------------------------------------------------------------------------------
+
